84c21b65d8f8ce74c32bf42d3a6281d3e5ca660e
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-17 13:35:04 -0400

    Successful Named Views Example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,78 @@
+<!-- EXAMPLE FROM; https://codesandbox.io/s/named-views-vue-router-4-examples-forked-qmt4pg?file=/src/router.js -->
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
+
+  <h1>Named Views</h1>
+  <ul>
+    <li>
+      <router-link to="/">First page</router-link>
+    </li>
+    <li>
+      <router-link to="/other">Second page</router-link>
+    </li>
+  </ul>
+  <router-view class="view one"></router-view>
+  <router-view class="view two" name="a"></router-view>
+  <router-view class="view three" name="b"></router-view>
 </template>
 
+<script>
+export default {
+  name: "App",
+};
+</script>
+
 <style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.router-link-active {
+  color: orange;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.router-link-exact-active {
+  color: crimson;
+}
+</style>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped>
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li:not(:last-of-type) {
+  margin-right: 1rem;
 }
 </style>
